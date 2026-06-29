b4cd891d5090a8f0deca51d844f532b3588efc15
hirroankit23-debug <hirro.ankit.23@gmail.com>
2026-06-29 21:24:45 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Predicts Yield (%) from process operating conditions.
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+
 import plotly.graph_objects as go
 
 # ----------------------------------------
