8aeef73dfc46a6cf10d2e533d15744c704e6695b
hirroankit23-debug <hirro.ankit.23@gmail.com>
2026-06-29 21:26:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ Predicts Yield (%) from process operating conditions.
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-
+import joblib
+import plotly.graph_objects as go
 
 # ----------------------------------------
 # Page Configuration
